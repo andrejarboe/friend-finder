45a059d5e78a58cfe1abb998251a5518adffaaf2
andrejarboe <andrejarboe@gmail.com>
2017-12-28 15:01:15 -0500

    add route for survey

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -14,6 +14,10 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
+    app.get("/survey", function(req, res){
+      res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
     // If no matching route is found default to home
     app.get("*", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/home.html"));

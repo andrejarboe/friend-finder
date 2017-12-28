c0f9b196e9db3b1f422a54a33b4cfe5be7355bec
andrejarboe <andrejarboe@gmail.com>
2017-12-28 12:37:55 -0500

    start api routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,17 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendsData = require("../data/friends");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function () {
+  app.get("/api/friends", function (req, res) {
+    res.json(friendsData);
+  });
+}

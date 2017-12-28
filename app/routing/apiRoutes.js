4e024c4e85b900eeb46731c743cb088ac9caf68c
andrejarboe <andrejarboe@gmail.com>
2017-12-28 14:41:01 -0500

    add post to apo route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,4 +14,8 @@ module.exports = function (app) {
   app.get("/api/friends", function (req, res) {
     res.json(friendsData);
   });
+
+  app.post("api/friends", function(req, res){
+    friendsData.push(req.body);
+  });
 }
